a9f7426e059e47fe594b0322d05e714c89578417
Jim Perry <mail@jim.nz>
2023-01-06 11:59:46 +1300

    nvim: tidy mappings

--- neovim/.config/nvim/lua/user/mappings.lua
+++ neovim/.config/nvim/lua/user/mappings.lua
@@ -1,29 +1,23 @@
 local opts = { noremap = true, silent = true }
-
-local term_opts = { silent = true }
-
--- Shorten function name
 local map = vim.keymap.set
 
--- Better window navigation
-map("n", "<C-h>", "<C-w>h", opts)
-map("n", "<C-j>", "<C-w>j", opts)
-map("n", "<C-k>", "<C-w>k", opts)
-map("n", "<C-l>", "<C-w>l", opts)
+map({"i","c","v"}, ",j", "<Esc>", opts, { desc = "Convenient escape sequence" })
+map("n", "<S-l>", ":bnext<CR>", opts, { desc = "Next buffer" })
+map("n", "<S-h>", ":bprevious<CR>", opts, { desc = "Previous buffer" })
+map("n", ",j", "<Nop>", opts) -- dont use combo in n mode
+map("n", "<CR>", ":w<CR>", opts, { desc = "Quick write" })
+map("n", "H", "^", opts, { desc = "Home row start of line" })
+map("n", "L", "$", opts, { desc = "Home row end of line" })
+map("v", "<", "<gv", opts, { desc = "Keep selection afer indent left" })
+map("v", ">", ">gv", opts, { desc = "Keep selection after indent right" })
+map("n", "gp", "`[v`]", opts, { desc = "Visually select last paste" })
+map("n", "Q", "<Nop>", opts, { desc = "" }) -- Ban ex mode
+map("n", "<Leader>R", ":so $MYVIMRC<CR>", opts, { desc = "Reload config" })
+map("n", "<Leader>v", ":e $MYVIMRC<CR>", opts, { desc = "Fast open config" })
+map("n", "Y", "y$", opts, { desc = "Yank to end of line" })
 
--- Resize with arrows
-map("n", "<C-Up>", ":resize -2<CR>", opts)
-map("n", "<C-Down>", ":resize +2<CR>", opts)
-map("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-map("n", "<C-Right>", ":vertical resize +2<CR>", opts)
-
--- Navigate buffers
-map("n", "<S-l>", ":bnext<CR>", opts)
-map("n", "<S-h>", ":bprevious<CR>", opts)
-
--- Press jk fast to exit insert mode 
-map("i", ",j", "<ESC>", opts)
-
--- Stay in indent mode
-map("v", "<", "<gv", opts)
-map("v", ">", ">gv", opts)
+-- TODO old maps which need a plugin
+--map("n", "cd", ":Gcd<CR>:pwd<CR>", opts, { desc = "" }) -- Change dir to git repo which contains current file (then pwd)
+--map("n", "<C-p>", ":GitFiles<CR>", opts, { desc = "" }) -- Open file from git repo
+--map("n", "<M-b>", ":Gblame<CR>", opts, { desc = "" }) -- Toggle git blame panel
+--map("n", "<M-u>", ":UndotreeToggle<CR>", opts, { desc = "" }) -- Toggle undo tree
